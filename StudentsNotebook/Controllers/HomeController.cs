a381878aa2483926e1ca1b2631b1ee09cff56826
Mariogr5 <mariogr1001@gmail.com>
2022-11-03 18:18:42 +0100

    MainPage

---
+++ StudentsNotebook/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentsNotebook.Models;
+using System.Diagnostics;
+
+namespace StudentsNotebook.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Kurs> curseslist;
+        public HomeController()
+        {
+            curseslist = new List<Kurs>()
+            {
+                new Kurs(1, "Stefan", "medium"),
+                new Kurs(2, "Wodard", "easy"),
+                new Kurs(3, "Howard", "hard"),
+            };
+
+
+        }
+        public IActionResult Allcurses()
+        {
+            return View(curseslist);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StudentsNotebook/Models/Kurs.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentsNotebook.Models
+{
+    public class Kurs
+    {
+        public Kurs(int id, string professor, string learndifficulty)
+        {
+            this.id = id;
+            this.professor = professor;
+            this.learndifficulty = learndifficulty;
+        }
+        public int id { get; set; }
+        public string professor { get; set; }
+        public string learndifficulty { get; set; }
+    }
+}
