2e29ffa136b6a66437027c3bb8ac9446148d9e0c
Mariogr5 <mariogr1001@gmail.com>
2022-11-05 15:12:17 +0100

    Akcja HTTPOST + Widoki Materials i Stwórzkurs

--- StudentsNotebook/Controllers/HomeController.cs
+++ StudentsNotebook/Controllers/HomeController.cs
@@ -7,37 +7,54 @@ namespace StudentsNotebook.Controllers
     public class HomeController : Controller
     {
         List<Kurs> curseslist;
+        Kurs sam = new Kurs(2,"","","","");
         public HomeController()
         {
             curseslist = new List<Kurs>()
             {
-                new Kurs(1, "Podstawy produkcji metaamfetaminy", "Włodzimierz Biały", "medium"),
-                new Kurs(2, "Picie dla zaawansowanych", "Jeremiasz Różowy", "easy"),
-                new Kurs(3, "Podstawy samoobrony", "Heniek Szreder", "hard"),
-                new Kurs(4, "Zarządzanie", "Gustaw Fring", "hard"),
-            };
-
-            
+                new Kurs(1, "Podstawy produkcji metaamfetaminy", "Włodzimierz Biały", "medium", "wzorynacalki.jpg"),
+                new Kurs(2, "Ziołoznawstwo 3.3", "Jeremiasz Różowy", "easy","wzorynacalki.jpg"),
+                new Kurs(3, "Podstawy samoobrony", "Heniek Szreder", "hard","wzorynacalki.jpg"),
+                new Kurs(4, "Zarządzanie", "Gustaw Fring", "hard","wzorynacalki.jpg"),
+            };    
         }
         public IActionResult Allcurses()
         {
             return View(curseslist);
         }
-        public IActionResult Opinion(int id)
-        {
-
-            return View();
-        }
         public IActionResult Index()
         {
             return View();
         }
-
+        public IActionResult Materials(int id)
+        {
+            var rzeczy = curseslist.FirstOrDefault(p => p.Id == id);
+            return View(rzeczy);
+        }
         public IActionResult Privacy()
         {
             return View();
         }
-
+        public IActionResult CreateKurs()
+        {
+            var liczba = curseslist.Count();
+            ViewBag.liczbakursow = liczba;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateKurs([Bind("Id,Coursename,Professor,Learndifficulty,Notes")] Kurs kursow)
+        {
+            if (ModelState.IsValid)
+            { 
+                curseslist.Add(kursow);
+                return View("Allcurses", curseslist);
+            }
+            else
+            {
+                return View(kursow);
+            }
+    }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ StudentsNotebook/Models/NewKurs.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsNotebook.Models
+{
+    public class NewKurs
+    {
+        [Required(ErrorMessage = "Wymagane ID")]
+        [DisplayName("ID")]
+        public int Kursid { get; set; }
+        [Required(ErrorMessage = "Nazwa kursu wymagana")]
+        [DisplayName("Nazwa")]
+        public string Kursname { get; set; }
+        [Required]
+        [DisplayName("Prowadzący")]
+        public string KursProfessor { get; set; }
+        [DisplayName("Trudność kursu")]
+        public string Kurslearndifficulty { get; set; }
+
+    }
+}
